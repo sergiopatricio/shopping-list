a7eefb1a8f937bdca595021b009080792b66f2c8
Sérgio Patrício <spatricio@gmail.com>
2020-01-19 20:03:18 +0000

    Refactor ajax responses

--- app/assets/javascripts/shopping_list.js
+++ app/assets/javascripts/shopping_list.js
@@ -1,11 +1,9 @@
 $(function() {
   $(document).on('ajax:success', '.shopping-cart-item .action', function(event, data) {
-    $(this).closest('.shopping-cart-item').html(event.detail[0].html)
+    $(this).closest('.shopping-cart-item').replaceWith(event.detail[0].html)
   });
 
   $(document).on('ajax:success', '.order-confirmation-item .action', function(event, data) {
-    var $container = $(this).closest('.order-confirmation-item'),
-        data = event.detail[0];
-    $container.html(data.html).toggleClass('confirmed', data.confirmed)
+    $(this).closest('.order-confirmation-item').replaceWith(event.detail[0].html)
   });
 });
