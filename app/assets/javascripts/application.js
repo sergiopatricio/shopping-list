1c4054e0da10eeb7968dabe9b9380dbed70ed75b
Sérgio Patrício <spatricio@gmail.com>
2020-04-17 15:36:25 +0100

    Redirect when ajax call fails due to unauthorised

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,15 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+
+$(document).ajaxError(function(event, jqxhr) {
+  if (jqxhr.status == 401) {
+    window.location = '/';
+  }
+});
+
+$(document).on('ajax:error', function(event) {
+  if (event.detail[2].status === 401) {
+    window.location = '/';
+  }
+});
