3f39a1bef22e6aa8ade4049d5df8812c3f930ff8
Sérgio Patrício <spatricio@gmail.com>
2020-11-15 17:11:32 +0000

    Save orders sort

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,8 @@ class OrdersController < ApplicationController
 
   def show
     items = current_user.items.to_buy.includes(:group).references(:groups).merge(Group.active)
-    @items = if params[:sort] == 'name'
+    @sort = items_sort
+    @items = if @sort == 'name'
                items.order(:name)
              else
                items.order('groups.position, items.type, items.position')
@@ -15,5 +16,12 @@ class OrdersController < ApplicationController
   def destroy
     current_user.items.update_all(confirmed: false)
     redirect_to order_path, notice: 'Confirmations cleared.'
+  end
+
+  private
+
+  def items_sort
+    current_user.save_preference(:order_sort, params[:sort]) if params[:sort].present?
+    current_user.preference_for(:order_sort)
   end
 end
