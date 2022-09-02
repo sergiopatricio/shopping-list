a006604a8f4bc6b8a8dad91dd0d1d57cda405569
Sérgio Patrício <spatricio@gmail.com>
2022-09-02 12:20:31 +0100

    Fix migration

--- db/migrate/20200405175924_add_user_id_to_groups_and_items.rb
+++ db/migrate/20200405175924_add_user_id_to_groups_and_items.rb
@@ -8,7 +8,7 @@ class AddUserIdToGroupsAndItems < ActiveRecord::Migration[6.0]
     up_only do
       user = User.first
       # Item::Base.update_all(user_id: user.id)
-      Group.update_all(user_id: user.id)
+      Group.update_all(user_id: user&.id)
     end
   end
 end
