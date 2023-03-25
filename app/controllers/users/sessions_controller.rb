a26cd2d885fcb9a44ad55cc6bb0b0bc1fe76f114
Sérgio Patrício <spatricio@gmail.com>
2023-03-25 19:19:09 +0000

    Remove unnessary code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,8 +2,4 @@
 
 class Users::SessionsController < Devise::SessionsController
   layout 'session'
-
-  def respond_to_on_destroy
-    redirect_to new_user_session_path, status: :see_other
-  end
 end
