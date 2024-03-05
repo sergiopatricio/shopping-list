8d0d53728bfb038cf95a3b23413186ea65f33569
André Rodrigues <afpr252@gmail.com>
2024-03-05 16:03:25 +0000

    Use app with single user, automatically logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery
 
   private
+
+  def current_user
+    @current_user = User::first()
+  end
 
   def current_account
     @current_account ||= current_user&.account
