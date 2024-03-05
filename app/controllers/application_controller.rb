2bd1a5acbe73d067b445161384773721815290d8
André Rodrigues <afpr252@gmail.com>
2024-03-05 16:03:25 +0000

    Use app with single user, automatically logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  include Devise::Controllers::Helpers
+
+  # before_action :authenticate_user!
+  before_action :log_in_with_default_user, unless: :user_signed_in?
   protect_from_forgery
 
   private
+
+  def log_in_with_default_user
+    user = User::first()
+    sign_in(user)
+  end
 
   def current_account
     @current_account ||= current_user&.account
