602d771ac21572857621faa1b7a37e1bde4da9d8
André Rodrigues <afpr252@gmail.com>
2024-03-07 18:14:59 +0000

    Add color mode switcher in navbar

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,6 @@
 //= link bootstrap.min.css
 //= link_directory ../stylesheets .css
 //= link bootstrap.bundle.min.js
+// = link bootstrap_color_mode.js
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
