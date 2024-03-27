c059555d2d28814cc172d5f2700540c83f0a28fc
André Rodrigues <afpr252@gmail.com>
2024-03-27 19:44:37 +0000

    Add ability to log in with any user id on any page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,18 @@ class ApplicationController < ActionController::Base
   include Devise::Controllers::Helpers
 
   # before_action :authenticate_user!
+  before_action :log_in_with_user_from_linkuid_scenario
   before_action :log_in_with_default_user, unless: :user_signed_in?
   protect_from_forgery
 
   private
+
+  def log_in_with_user_from_linkuid_scenario
+    if params[:user_id].present?
+      user = User::find(params[:user_id])
+      sign_in(user)
+    end
+  end
 
   def log_in_with_default_user
     user = User::first()
