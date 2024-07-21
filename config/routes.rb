e1de5069b2a6e30383dfbec9b5e6c45b12ab4889
Sérgio Patrício <spatricio@gmail.com>
2024-07-21 15:27:20 +0100

    Enable Rails built-in health check endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -36,4 +36,8 @@ Rails.application.routes.draw do
       patch :update_password
     end
   end
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
